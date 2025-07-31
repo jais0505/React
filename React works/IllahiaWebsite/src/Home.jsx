709f08c0c3ac2a1f839c8f524194d91a93b3d6a9
Jais Jose <jaisjose44@gmail.com>
2025-07-31 07:59:36 +0530

    Ilahia website footer desinging

--- React works/IllahiaWebsite/src/Home.jsx
+++ React works/IllahiaWebsite/src/Home.jsx
@@ -3,6 +3,7 @@ import styles from './home.module.css'
 import { FaPhone } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
+import { FaPhoneAlt } from "react-icons/fa";
 const HomePage = () =>{
     return(
         <div>
@@ -64,29 +65,53 @@ const HomePage = () =>{
             <div className={styles.headlinediv}>
                 <div style={{margin:"15px",display: "flex",gap:"20px"}}>
                 <div className={styles.headlineitems1}>HEADLINES &gt;&gt;&gt;</div>  
-                <div className={styles.headlineitems}>COLLEGE DAY CELEBRATION</div>|
-                <div className={styles.headlineitems}>ANNUAL SPORTS MEET 2025</div>|
-                <div className={styles.headlineitems}>COLLEGE ARTS DAY 2025</div>|
-                <div className={styles.headlineitems}>AKHDAR FILM FESTIVAL</div>
+                    <div className={styles.headlineitems}>COLLEGE DAY CELEBRATION</div>|
+                    <div className={styles.headlineitems}>ANNUAL SPORTS MEET 2025</div>|
+                    <div className={styles.headlineitems}>COLLEGE ARTS DAY 2025</div>|
+                    <div className={styles.headlineitems}>AKHDAR FILM FESTIVAL</div>
+                </div>
+            </div>
+            <div className={styles.area1}>
+                <div className={styles.area1box1}>
+
+                </div>
+                <div className={styles.area1imgbox}>
+
                 </div>
             </div>
             <div className={styles.footer}>
                 <div className={styles.footertop}>
 
                 </div>
-                <hr />
+                <hr style={{ border: "0.3px solid #ccc", margin: "5px 0" }} />
                 <div className={styles.footercenter}>
                     <div className={styles.footercenteritem1} >
-                        <div>
-                            <FaPhone style={{ color: "#1755a7", fontSize: "30px" }} />
+                        <div className={styles.item1icon}>
+                            <FaPhoneAlt style={{ color: "#1755a7", fontSize: "30px" }} />
+                        </div>
+                        <div className={styles.item1txt}>
+                            <div className={styles.item1call}>Call us at</div>
+                            <div className={styles.item1no}>0485-2811607</div>
                         </div>
                     </div>
-                    {/* <div>
-                        <FaLocationDot style={{ color: "#1755a7", fontSize: "30px" }} />
+                    <div className={styles.footercenteritem2} >
+                        <div className={styles.item2icon}>
+                            <FaLocationDot style={{ color: "#1755a7", fontSize: "30px" }} />
+                        </div>
+                        <div className={styles.item2txt}>
+                            <div className={styles.item2call}>Visit us</div>
+                            <div className={styles.item2no}>Muvattupuzha</div>
+                        </div>
                     </div>
-                    <div>
-                        <IoMdMail style={{ color: "#1755a7", fontSize: "30px" }} />
-                    </div> */}
+                    <div className={styles.footercenteritem3} >
+                        <div className={styles.item3icon}>
+                             <IoMdMail style={{ color: "#1755a7", fontSize: "30px" }} />
+                        </div>
+                        <div className={styles.item3txt}>
+                            <div className={styles.item3call}>Email us at</div>
+                            <div className={styles.item3no}>ilahiaartscollege@gmail.com</div>
+                        </div>
+                    </div>
                 </div>
                 <div className={styles.footerbottom}>
                     <div className={styles.fotterbottomtxt}>© 2024 Ilahia College of Arts & Science. All rights reserved.</div>
